eff7f4ad1e8c024d57d0929f79a9a24450797010
声微 <756933962@qq.com>
2021-12-26 15:22:09 +0800

    day3

--- 列表.py
+++ 列表.py
@@ -1,6 +1,44 @@
 # print('hello %s %d'%('world',123))
-L=[1,'二',True,2.1,[3,4]]
+L=[1,'二',True,2.1,3.1,[3,4]]
 print(L)
 print(L[0])
 print(L[-1])# 反向取
 print(L[-1][1])
+#计算列表元素个数总数
+print(len(L))
+#成员运算in， not in
+print('二'in L)
+print(1 not in L)
+#查看某个元素的个数count
+print(L.count(1))
+#查询元素，起始位置，结束位置，index
+print(L.index('二'))#不存在会报错
+#增加
+#append(元素)忘列表末尾追加一个元素
+L.append(9)
+print(L)
+#insert(元素，位置)往指定位置插入一个元素
+L.insert(0,'hell0')
+print(L)
+#extend()往末尾添加多个元素，括号里放列表，末尾追加
+L.extend([8,8])
+print(L)
+# 删除del[索引]
+L.remove(8)#从后往前删第一个
+print(L)
+res=L.pop(2)
+print(res,L)
+# 改
+L[0]=9
+print(L)
+# 清空列表
+#L.clear()
+# 反序
+L.reverse()
+print(L)
+# 排序(纯数字列表）
+lm=[1,3,2,4,7,6]
+lm.sort(reverse=True)
+print(lm)
+lm.sort(reverse=False)
+print(lm)
--- 字符串.py
+++ 字符串.py
@@ -4,3 +4,70 @@ print(msg[0])
 print(msg[0:4])# 顾头不顾腚
 print(msg[0:6:2])
 print(msg[6:0:-1])
+print(len(msg))# 字符串长度计算
+print('hello'in msg)#字符串逻辑判断
+print('hello'not in msg)
+#字符串方法
+# 增加，拼接
+print('a'+'b')
+#format
+print('my name is {}'.format(msg))
+print('my name is {1},my age is {0}'.format(18,'sb'))
+print('my name is {name},my age is {age}'.format(name='sb',age='18'))
+# join
+s1='我'
+s2='爱'
+s3='你'
+print(''.join([s1,s2,s3]))
+print('!'.join([s1,s2,s3]))
+# 改大小写
+msg2='aBc'
+msg3=msg2.upper()
+msg4=msg2.lower()
+print(msg3,msg4)
+# 单词首字母变大写capitalize
+letter='hello world'
+print(letter.capitalize())# 首个单词
+print(letter.title())# 每个单词
+# 切分单词，默认按空格切split
+msg5='hello my world'
+print(msg5.split())
+msg6='H0M0W'
+print(msg6.split('0'))
+
+mm='密码:123456'
+key=mm.split(':')
+print(key[1])
+
+# 去掉字符串两边的字符，不写默认空格，不管中间字符
+user='      sb     '
+print('sb'==user.strip())
+# 多余添加自己需要的字符
+print('sb'.center(10,'*'))
+print('sb'.rjust(10,'*'))
+print('sb'.ljust(10,'*'))
+# 查子字符串的位置（起始索引）
+t='hello world'
+print(t.find('ll'))
+print(t.find('ll',3,6))# 找不到返回-1
+#判断一个字符串里的数据是不是都是数字isdigit
+num='1818'
+print(num.isdigit())
+#判断是不是都是字母isalpha
+print(num.isalpha())
+# 比较开头元素是否相同startswith,比较结尾enswith
+hello='hello world'
+print(hello.startswith('he'))
+print(hello.startswith('e'))
+print(hello.endswith('d'))
+print(hello.endswith('l'))
+#判断是否全是小写/大写，islower/isupper
+
+#字符串的转义
+#加'\'的字符不再表示它原本的含义
+print('hello\nworld')#换行
+print('hello\tworld')#tab 4个空格
+#字符串的反转义，\n就是\n
+print(r'hello\nworld')
+
+
